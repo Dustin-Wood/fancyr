af4315c253fd4cf97fdb23d4bec248c8574df2ae
Dustin Wood <84573697+funfield-lab@users.noreply.github.com>
2021-10-26 11:56:28 +0300

    removed 'pervVar2', which became former 'perVar'

--- R/perVar.R
+++ R/perVar.R
@@ -2,23 +2,25 @@
 #' @description
 #' For a specified set of variables (with a common range), estimate the percentage
 #' of response variability that the person showed from maximum possible
-#' @param data Matrix to be transformed
-#' @param varSet Set of variables to estimate variance over (note: should all have same range)
-#' @param smax Scale maximum
+#' @param data Matrix to be transformed (often will need to specify subset of larger dataframe)
 #' @param smin Scale minimum
+#' @param smax Scale maximum
 #' @details This is frequently valuable to identify people who did not
 #' vary their ratings substantially - which at extreme levels is generally indicative
 #' of insufficient effort responding.
+#'
+#' Note: this will currently provide inaccurate estimates for respondents
+#' with missing values within the profile, so it is valuable to remove
+#' respondents with large number of missing values
 #' @return estimate of the proportion of the responding from max possible (range from 0 to 1)
 #' @export
 #' @examples
 #' #combine with subset function to remove people with less than 5%
 #' #of the maximum possible variance over this range (an indicator of invariant responding)
-#' data$perVar <- perVar(data, varSet, 1, 5)
-#' subdata <- subset(data, perVar > .05)
+#' datafile$perVar <- perVar(datafile[varSet], 1, 5)
+#' subdata <- subset(datafile, perVar > .05)
 
-#Remove cases with negligible response variance
-perVar<-function (data, varSet, smax, smin) {
-  perMaxVar <- apply(data[varSet], 1, function(x) (sd(as.matrix(x, na.rm = F)) * (sqrt((length(x) - 1)/length(x)))/(smax - smin)/(0.5))^2)
+perVar<-function (data, smin, smax) {
+  perMaxVar <- apply(data, 1, function(x) (sd(as.matrix(x), na.rm = T) * (sqrt((length(x) - 1)/length(x)))/(smax - smin)/(0.5))^2)
   return(perMaxVar)
 }
