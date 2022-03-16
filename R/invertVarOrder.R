9f748b169c67997a0168c12604d1acf9cb4f410b
Dustin Wood <84573697+funfield-lab@users.noreply.github.com>
2022-03-16 09:54:26 +0300

    made invertVarOrder function more flexible for named variables in item order listing

--- R/invertVarOrder.R
+++ R/invertVarOrder.R
@@ -5,13 +5,17 @@
 #' item #1, #2, etc.
 #' @param pByOrder dataframe containing participant ID as column 1, and then columns describing
 #' which item was rated first, second, etc
+#' @param vOrder specify the order you would like the variables to be ordered (this is necessarily
+#' mainly if the cells of the pByOrder matrix are variable names rather than variable NUMBERS)
 #' @usage
 #' invertVarOrder(pByOrder)
+#' invertVarOrder(pByOrder, vorder = pscores)
 #' @return A participant-by-item matrix detailing when participant rated each item
 #'
 #' @export
 
-invertVarOrder <- function(pByOrder){
+
+invertVarOrder <- function(pByOrder, vorder = NA){
   longMat<-tidyr::pivot_longer(pByOrder, cols = 2:ncol(pByOrder), names_to = "order", values_to = "var")
 
   #remove problematic X's and V's that will get in the way of making scores numeric
@@ -23,7 +27,11 @@ invertVarOrder <- function(pByOrder){
   longMat<-longMat[order(longMat$var),]
   #remove any missing values before pivoting
   longMat <- subset(longMat, is.na(var)==0)
+  longMat <- subset(longMat, var != "")
 
   pByVar <- as.data.frame(tidyr::pivot_wider(longMat, names_from = var, values_from = order))
+  if(sum(is.na(vorder)) == 0){
+    pByVar<-pByVar[,vorder]
+  }
   return(pByVar)
 }
