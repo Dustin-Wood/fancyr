bf9b82a7e4da7e6f3bbfbcc4b0f3c640e9dfaea3
Dustin Wood <84573697+funfield-lab@users.noreply.github.com>
2022-07-14 16:50:53 +0300

    simplified ccor function to make it usually a bit easier to use

--- R/ccor.R
+++ R/ccor.R
@@ -6,11 +6,10 @@
 #' These are equivalent to Cohen's (1969) rc correlations,
 #' which are invariant across potentially arbitrary item- or variable-reversals.
 #' @param x matrix to be used for correlations
-#' @param smin Scale minimum (default if not given = -1)
-#' @param smax Scale maximum (default if not given = 1)
+#' @param mid Scale midpoint (default if not given = 0)
 #' @param margin Specify whether correlations are of rows=1 or columns = 2 (default is columns)
 #' @usage
-#' ccor(x, smin = 1, smax = 6) #e.g., standard 1-5 Likert scale
+#' ccor(x, mid = 3) #e.g., midpoint of standard 1-5 Likert scale is 3
 #' @details
 #' Function computes Cohen-adjusted correlations (or avg. product of standardized deviations-from-scale-center)
 #' @return Scale-center correlations (\code{r_c})
@@ -20,8 +19,8 @@
 #' friendly toward missing values.
 #' @export
 
-ccor <- function(x, smin = -1, smax = 1, margin=2) {
-  cx <- ((x - (smax + smin)/2)/(smax-smin))*2
+ccor <- function(x, mid = 0, margin=2) {
+  cx <- x - mid
   zcx <- apply(cx, margin, function(x) (x/sqrt((sum(x^2)/(length(x))))))
   ccor <- t(zcx) %*% zcx / nrow(zcx) #this last bit can be used to calculate matching pretty generally
   dimnames(ccor)<-list(colnames(x),colnames(x))
